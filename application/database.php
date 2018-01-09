35381336ab6ec8567c0afc45dd8432beaff8165c
testYoYo <sinian1860@126.com>
2018-01-09 13:34:38 +0800

    rename filename

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'           => 'sqlsrv',
     // 服务器地址
-    'hostname'       => 'localhost',
+    'hostname'       => '47.95.230.158',
     // 数据库名
     'database'       => 'hxlz',
     // 用户名
